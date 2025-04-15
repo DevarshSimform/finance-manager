c3904ee27fa8f2b57a2999d09a599598cb5f3a2a
Devarsh <devarsh.chhatrala@simformsolutions.com>
2025-04-15 14:39:17 +0530

    Added background task - sending mail of daily transaction summary

--- core/celery.py
+++ core/celery.py
@@ -6,3 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+from celery.schedules import crontab
+
+app.conf.beat_schedule = {
+    'send-daily-transaction-email': {
+        'task': 'finance.tasks.send_daily_transaction_email',
+        'schedule': crontab(hour=9, minute=00),  # Every day at 9 AM
+    },
+}
