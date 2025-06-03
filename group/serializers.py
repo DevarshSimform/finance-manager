0a8dcd6d0a439c5411bf92eb00f40674c7f8cde1
Devarsh <devarsh.chhatrala@simformsolutions.com>
2025-06-03 17:37:16 +0530

    Added creation and listing for groups

--- group/serializers.py
+++ group/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from finance.models import CustomUser
 from group.models import Group, GroupMember
 
 
@@ -9,13 +10,43 @@ class GroupMemberSerializer(serializers.ModelSerializer):
         model = GroupMember
         fields = ['user', 'joined_at']
 
+
 class GroupSerializer(serializers.ModelSerializer):
-    members = serializers.SerializerMethodField()
+    # members = serializers.SerializerMethodField()
+    members = GroupMemberSerializer(many=True, read_only=True)
 
     class Meta:
         model = Group
         fields = '__all__'
 
-    def get_members(self, obj):
-        members = GroupMember.objects.filter(group=obj)
-        return GroupMemberSerializer(members, many=True).data
+    # def get_members(self, obj):
+    #     members = GroupMember.objects.filter(group=obj)
+    #     return GroupMemberSerializer(members, many=True).data
+
+
+class GroupCreateSerializer(serializers.ModelSerializer):
+    members = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = Group
+        fields = ['name', 'members']
+
+    def create(self, validated_data):
+        members = validated_data.pop('members', [])
+        # user = self.context['request'].user
+        
+        # Hardcoding default user because AllowAny permission defined in view
+        user = CustomUser.objects.get(id=15)
+        validated_data['created_by'] = user
+        group = Group.objects.create(**validated_data)
+
+        for member_id in members:
+            try:
+                member = CustomUser.objects.get(id=member_id)
+                GroupMember.objects.create(user=member, group=group)
+            except CustomUser.DoesNotExist:
+                continue
+
+        return group
--- group/views.py
+++ group/views.py
@@ -10,11 +10,17 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from group.models import (
     Group,
 )
-from group.serializers import GroupSerializer
+from group.serializers import GroupSerializer, GroupCreateSerializer
 
 
 class GroupListCreateAPIView(ListCreateAPIView):
 
     queryset = Group.objects.all()
     permission_classes = [AllowAny]
-    serializer_class = GroupSerializer
+    # serializer_class = GroupSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return GroupCreateSerializer
+        else:
+            return GroupSerializer
